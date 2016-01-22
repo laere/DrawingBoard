9073146250d6212de64ccb3113e1e41e0e890767
Zack Bostian <bostianz@gmail.com>
2016-01-22 11:43:40 -0500

    added the color picker side bar

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(function() {
+
+  //TOGGLE COLOR PICKER!
+  //When color picker is color-picker-body is clicked
+    //show color picker;
+    //when clicked again, hide color picker.
+
+
+
+
+
+})(jQuery);
