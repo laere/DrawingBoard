ad3b58d38fa381f0da7fa707c2060e0097bd6f82
Zack Bostian <bostianz@gmail.com>
2016-01-22 22:42:40 -0500

    added toggle to color picker

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 $(function() {
-
+  $colorPicker = $('#color-picker');
   //TOGGLE COLOR PICKER!
   //When color picker is color-picker-body is clicked
-    //show color picker;
-    //when clicked again, hide color picker.
-
+  //show color picker;
+  //when clicked again, hide color picker.
+$('#color-picker-body').click(function() {
+  $colorPicker.toggle('fast');
+});
 
 
 
